b703c2ec86d03bb96aa6d57c420487e70a2d8c69
BrunaDan7as <b.dantassoares@gmail.com>
2025-01-02 23:04:39 -0300

    GET e UPDATE feitos

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from models.task import Task
-
 app = Flask(__name__)
 
 # CRUD
 # Create, Read, Update and Delete = Criar, Ler, Atualizar, Deletar
 # Tabela: Tarefa
+
 tasks = []
 task_id_control = 1
 
@@ -19,5 +19,20 @@ def create_task():
   print(tasks)
   return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  }
+  return jsonify(output)
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+  
+  return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
